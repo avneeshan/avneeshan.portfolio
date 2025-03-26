c6028aa51e9eb5280b03aa6bd79b9de810a9b025
Avneeshan <78402132+avneeshan@users.noreply.github.com>
2025-03-26 21:59:48 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", function () {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const name = document.querySelector("#fullname").value;
+    const name = document.querySelector("#name").value;
     const email = document.querySelector("#email").value;
     const message = document.querySelector("#message").value;
 
